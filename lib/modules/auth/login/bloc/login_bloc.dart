5c0bbbfd730de25899bc6e79efff1e90097a26c8
HassanSobhy <hassansobhy60@gmail.com>
2021-07-20 02:40:09 +0200

    add login repository

--- lib/modules/auth/login/bloc/login_bloc.dart
+++ lib/modules/auth/login/bloc/login_bloc.dart
@@ -1,15 +1,19 @@
 import 'package:bloc/bloc.dart';
+import 'package:shop_app/network/repository/login_repository.dart';
 
 import 'login_event.dart';
 import 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent,LoginState>{
-
-  LoginBloc() : super(LoginInitialState());
+  final LoginRepository loginRepository;
+  LoginBloc(this.loginRepository) : super(LoginInitialState());
 
   @override
-  Stream<LoginState> mapEventToState(LoginEvent event) {
-    
+  Stream<LoginState> mapEventToState(LoginEvent event)async* {
+    if(event is UserLoginEvent){
+      yield LoginLoadingState();
+      yield await loginRepository.signInWithEmailAndPassword(event.loginModel);
+    }
   }
 
 }
