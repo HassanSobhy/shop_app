bfaab48a28683eb87c18b8048746c9212a7a5716
HassanSobhy <hassansobhy60@gmail.com>
2021-07-29 09:16:11 +0200

    move validation to bloc

--- lib/modules/auth/login/login_screen.dart
+++ lib/modules/auth/login/login_screen.dart
@@ -13,12 +13,11 @@ import 'bloc/login_event.dart';
 import 'bloc/login_state.dart';
 
 class LoginScreen extends StatelessWidget {
-  //Form Key
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-
   //TextFormFieldControllers
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  String emailErrorMessage;
+  String passwordErrorMessage;
 
   @override
   Widget build(BuildContext context) {
@@ -33,108 +32,95 @@ class LoginScreen extends StatelessWidget {
               navigateToHomeScreen(context);
             } else if (state is LoginErrorState) {
               buildToastMessage(state.message, Colors.red);
+            } else if (state is LoginEmailAndPasswordValidationState){
+              emailErrorMessage = state.emailMessage;
+              passwordErrorMessage =  state.passwordMessage;
             }
           },
           builder: (context, state) {
             return SingleChildScrollView(
               child: Padding(
                 padding: const EdgeInsets.all(16.0),
-                child: Form(
-                  key: formKey,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Login",
-                        style: Theme.of(context).textTheme.headline4.copyWith(
-                            fontWeight: FontWeight.bold, color: Colors.black),
-                      ),
-                      Text("Login now to browse our hot offers"),
-                      SizedBox(height: 40),
-                      TextFormField(
-                        textInputAction: TextInputAction.next,
-                        controller: emailController,
-                        validator: (String value) {
-                          if (value.isEmpty) {
-                            return "Email Shouldn't be Empty";
-                          } else
-                            return null;
-                        },
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: InputDecoration(
-                          labelText: "Email",
-                          prefixIcon: Icon(Icons.email_outlined),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Login",
+                      style: Theme.of(context).textTheme.headline4.copyWith(
+                          fontWeight: FontWeight.bold, color: Colors.black),
+                    ),
+                    Text("Login now to browse our hot offers"),
+                    SizedBox(height: 40),
+                    TextFormField(
+                      textInputAction: TextInputAction.next,
+                      controller: emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        errorText: emailErrorMessage,
+                        prefixIcon: Icon(Icons.email_outlined),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
                         ),
                       ),
-                      SizedBox(height: 16),
-                      TextFormField(
-                        textInputAction: TextInputAction.done,
-                        keyboardType: TextInputType.text,
-                        obscureText: LoginBloc.get(context).isPassword,
-                        controller: passwordController,
-                        validator: (String value) {
-                          if (value.isEmpty) {
-                            return "Password shouldn't be Empty";
-                          } else
-                            return null;
-                        },
-                        decoration: InputDecoration(
-                          labelText: "Password",
-                          prefixIcon: Icon(Icons.lock_outline_rounded),
-                          suffixIcon: IconButton(
-                            icon: Icon(LoginBloc.get(context).suffix),
+                    ),
+                    SizedBox(height: 16),
+                    TextFormField(
+                      textInputAction: TextInputAction.done,
+                      keyboardType: TextInputType.text,
+                      obscureText: LoginBloc.get(context).isPassword,
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        errorText: passwordErrorMessage,
+                        prefixIcon: Icon(Icons.lock_outline_rounded),
+                        suffixIcon: IconButton(
+                          icon: Icon(LoginBloc.get(context).suffix),
+                          onPressed: () {
+                            changePasswordIconVisibility(context);
+                          },
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 32),
+                    ConditionalBuilder(
+                      condition: state is! LoginLoadingState,
+                      builder: (context) {
+                        return Container(
+                          height: 45,
+                          width: double.infinity,
+                          child: ElevatedButton(
                             onPressed: () {
-                              changePasswordIconVisibility(context);
+                              LoginModel loginModel = LoginModel(
+                                emailController.text.trim(),
+                                passwordController.text.trim(),
+                              );
+                              userLogin(context, loginModel);
                             },
+                            child: Text("LOGIN"),
                           ),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 32),
-                      ConditionalBuilder(
-                        condition: state is! LoginLoadingState,
-                        builder: (context) {
-                          return Container(
-                            height: 45,
-                            width: double.infinity,
-                            child: ElevatedButton(
-                              onPressed: () {
-                                if (formKey.currentState.validate()) {
-                                  LoginModel loginModel = LoginModel(
-                                    emailController.text.trim(),
-                                    passwordController.text.trim(),
-                                  );
-                                  userLogin(context, loginModel);
-                                }
-                              },
-                              child: Text("LOGIN"),
-                            ),
-                          );
-                        },
-                        fallback: (context) => buildLoadingState(),
-                      ),
-                      SizedBox(height: 16),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text("Don't have an account?"),
-                          TextButton(
-                              onPressed: () =>
-                                  navigateToRegisterScreen(context),
-                              child: Text(
-                                "Register",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 14),
-                              )),
-                        ],
-                      )
-                    ],
-                  ),
+                        );
+                      },
+                      fallback: (context) => buildLoadingState(),
+                    ),
+                    SizedBox(height: 16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Don't have an account?"),
+                        TextButton(
+                            onPressed: () => navigateToRegisterScreen(context),
+                            child: Text(
+                              "Register",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 14),
+                            )),
+                      ],
+                    )
+                  ],
                 ),
               ),
             );
