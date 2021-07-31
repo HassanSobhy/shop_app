c1bff72e3bcfcbcac87f846a756eb54bf9adc981
HassanSobhy <hassansobhy60@gmail.com>
2021-07-31 11:10:01 +0200

    add register_user_data_model file

--- lib/models/register/register_response_model.dart
+++ lib/models/register/register_response_model.dart
@@ -1,36 +1,14 @@
+import 'package:shop_app/models/register/register_user_data_model.dart';
+
 class ResponseModel{
   bool status;
   String message;
-  UserData data;
+  RegisterUserDataModel data;
 
   ResponseModel.fromJson(Map<String, dynamic> json)
   {
     status = json['status'];
     message = json['message'];
-    data = json['data'] != null ? UserData.fromJson(json['data']) : null;
-  }
-}
-
-class UserData{
-  int id;
-  String name;
-  String email;
-  String phone;
-  String image;
-  int points;
-  int credit;
-  String token;
-
-  // named constructor
-  UserData.fromJson(Map<String, dynamic> json)
-  {
-    id = json['id'];
-    name = json['name'];
-    email = json['email'];
-    phone = json['phone'];
-    image = json['image'];
-    points = json['points'];
-    credit = json['credit'];
-    token = json['token'];
+    data = json['data'] != null ? RegisterUserDataModel.fromJson(json['data']) : null;
   }
 }
---
+++ lib/models/register/register_user_data_model.dart
@@ -0,0 +1,18 @@
+class RegisterUserDataModel{
+  int id;
+  String name;
+  String email;
+  String phone;
+  String image;
+  String token;
+
+  RegisterUserDataModel.fromJson(Map<String, dynamic> json)
+  {
+    id = json['id'];
+    name = json['name'];
+    email = json['email'];
+    phone = json['phone'];
+    image = json['image'];
+    token = json['token'];
+  }
+}
