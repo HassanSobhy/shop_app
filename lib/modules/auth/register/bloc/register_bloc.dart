a197c2116cc10863833d67273c85065706d541d3
HassanSobhy <hassansobhy60@gmail.com>
2021-07-31 12:39:40 +0200

    write the business logic in register bloc file

--- lib/modules/auth/register/bloc/register_bloc.dart
+++ lib/modules/auth/register/bloc/register_bloc.dart
@@ -1,18 +1,47 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:shop_app/models/register/register_model.dart';
+import 'package:shop_app/models/register/register_response_model.dart';
+import 'package:shop_app/modules/auth/register/bloc/register_repository.dart';
 
 part 'register_event.dart';
+
 part 'register_state.dart';
 
 class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
-  RegisterBloc() : super(RegisterInitial());
+  final BaseRegisterRepository registerRepository;
+  IconData suffix = Icons.visibility_outlined;
+  bool isPassword = true;
+
+  RegisterBloc(this.registerRepository) : super(const RegisterInitialState());
+
+  static RegisterBloc get(BuildContext context) =>
+      BlocProvider.of<RegisterBloc>(context);
 
   @override
   Stream<RegisterState> mapEventToState(
     RegisterEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    if (event is UserRegisterEvent) {
+      yield const RegisterLoadingState();
+      yield await registerRepository
+          .signUpWithEmailAndPassword(event.registerModel);
+    } else if (event is RegisterNavigationToLoginScreenState) {
+      yield const RegisterNavigationToLoginScreenState();
+    } else if (event is ChangePasswordVisibilityEvent) {
+      yield changePasswordVisibility();
+    }
+  }
+
+  RegisterChangePasswordVisibilityState changePasswordVisibility() {
+    isPassword = !isPassword;
+    suffix =
+    isPassword ? Icons.visibility_outlined : Icons.visibility_off_outlined;
+
+    return RegisterChangePasswordVisibilityState();
   }
 }
--- lib/modules/auth/register/bloc/register_state.dart
+++ lib/modules/auth/register/bloc/register_state.dart
@@ -1,6 +1,48 @@
 part of 'register_bloc.dart';
 
 @immutable
-abstract class RegisterState {}
+abstract class RegisterState {
+  const RegisterState();
+}
 
-class RegisterInitial extends RegisterState {}
+class RegisterInitialState extends RegisterState {
+  const RegisterInitialState();
+}
+
+class RegisterLoadingState extends RegisterState {
+  const RegisterLoadingState();
+}
+
+class RegisterSuccessState extends RegisterState {
+  final RegisterResponseModel model;
+
+  const RegisterSuccessState(this.model);
+}
+
+class RegisterErrorState extends RegisterState {
+  final String message;
+
+  const RegisterErrorState(this.message);
+}
+
+class RegisterChangePasswordVisibilityState extends RegisterState {
+  const RegisterChangePasswordVisibilityState();
+}
+
+class RegisterNavigationToLoginScreenState extends RegisterState{
+  const RegisterNavigationToLoginScreenState();
+}
+
+class RegisterFormValidationState extends RegisterState {
+  final String userNameMessage;
+  final String emailMessage;
+  final String passwordMessage;
+  final String phoneMessage;
+
+  const RegisterFormValidationState(
+    this.userNameMessage,
+    this.emailMessage,
+    this.passwordMessage,
+    this.phoneMessage,
+  );
+}
