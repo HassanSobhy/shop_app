2ee3ee728b0670df80c24c0c7b09222b75bc4ec2
HassanSobhy <hassansobhy60@gmail.com>
2021-08-09 16:18:09 +0200

    add bottom navigation events

--- lib/modules/home/bloc/bottom_navigation_bar_event.dart
+++ lib/modules/home/bloc/bottom_navigation_bar_event.dart
@@ -2,3 +2,11 @@ part of 'bottom_navigation_bar_bloc.dart';
 
 @immutable
 abstract class BottomNavigationBarEvent {}
+
+class NavigationToProductScreenEvent extends BottomNavigationBarEvent {}
+
+class NavigationToCategoriesScreenEvent extends BottomNavigationBarEvent {}
+
+class NavigationToFavoritesScreenEvent extends BottomNavigationBarEvent {}
+
+class NavigationToSettingsScreenEvent extends BottomNavigationBarEvent {}
