eeb6d78676bf09ee79d1a2af5af91a150007b3ff
HassanSobhy <hassansobhy60@gmail.com>
2021-08-11 18:39:25 +0200

    solve conflict

--- lib/network/remote/auth_api_service.dart
+++ lib/network/remote/auth_api_service.dart
@@ -3,10 +3,7 @@ import 'package:flutter/foundation.dart';
 import 'package:dio/dio.dart';
 import 'package:shop_app/network/remote/dio_helper.dart';
 
-<<<<<<< HEAD:lib/network/remote/auth_api_service.dart
 class AuthApiService {
-=======
-class LoginApiService {
   static Future<Response> signInWithEmailAndPassword(
       {@required String path,
       @required Map<String, dynamic> data,
@@ -15,21 +12,6 @@ class LoginApiService {
       'lang': lang,
     };
     return DioHelper.dio.post(path, data: data);
-  }
-
->>>>>>> development:lib/network/remote/login_api_service.dart
-  static Future<Response> postData({
-    @required String path,
-    @required Map<String, dynamic> data,
-    Map<String, dynamic> query,
-    String lang = 'en',
-    String token,
-  }) async {
-    DioHelper.dio.options.headers = {
-      'lang': lang,
-      'Authorization': token,
-    };
-    return DioHelper.dio.post(path, data: data, queryParameters: query);
   }
 
   static Future<Response> signUpWithEmailAndPassword({
