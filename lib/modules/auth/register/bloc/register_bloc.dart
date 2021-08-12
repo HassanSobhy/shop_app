75cca2cb33270aad1ec505a77fa94ac42b1e92d0
HassanSobhy <hassansobhy60@gmail.com>
2021-08-12 04:38:18 +0200

    finish register bloc

--- lib/modules/auth/register/bloc/register_bloc.dart
+++ lib/modules/auth/register/bloc/register_bloc.dart
@@ -4,9 +4,11 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
-import 'package:shop_app/models/register/register_model.dart';
+
 import 'package:shop_app/models/register/register_response_model.dart';
+import 'package:shop_app/models/register/register_model.dart';
 import 'package:shop_app/modules/auth/register/bloc/register_repository.dart';
+import 'package:shop_app/utils/validator.dart';
 
 part 'register_event.dart';
 
@@ -27,14 +29,94 @@ class RegisterBloc extends Bloc<RegisterEvent, RegisterState> {
     RegisterEvent event,
   ) async* {
     if (event is UserRegisterEvent) {
-      yield const RegisterLoadingState();
-      yield await registerRepository
-          .signUpWithEmailAndPassword(event.registerModel);
+      yield* handelRegisterEvent(event.registerModel);
     } else if (event is RegisterNavigationToLoginScreenState) {
       yield const RegisterNavigationToLoginScreenState();
     } else if (event is ChangePasswordVisibilityEvent) {
       yield changePasswordVisibility();
     }
+  }
+
+  Stream<RegisterState> handelRegisterEvent(
+      RegisterModel registerModel) async* {
+    final RegisterState userNameState =
+        validateUserName(registerModel.username);
+    if (userNameState is RegisterUsernameFormatCorrectState) {
+      final RegisterState emailState = validateEmail(registerModel.email);
+      if (emailState is RegisterEmailFormatCorrectState) {
+        final RegisterState passwordState =
+            validateEmail(registerModel.password);
+        if (passwordState is RegisterPasswordFormatCorrectState) {
+          final RegisterState phoneState = validatePhone(registerModel.phone);
+          if (phoneState is RegisterPhoneFormatCorrectState) {
+            yield const RegisterLoadingState();
+            yield await registerRepository
+                .signUpWithEmailAndPassword(registerModel);
+          } else {
+            yield phoneState;
+          }
+        } else {
+          yield passwordState;
+        }
+      } else {
+        yield emailState;
+      }
+    } else {
+      yield userNameState;
+    }
+  }
+
+  RegisterState validateUserName(String userName) {
+    RegisterState registerState;
+    final ValidationState validateState = Validator.validateUserName(userName);
+    if (validateState == ValidationState.Empty) {
+      registerState = RegisterUsernameEmptyFormatState();
+    } else if (validateState == ValidationState.Formatting) {
+      registerState = RegisterUsernameInvalidFormatState();
+    } else {
+      registerState = RegisterUsernameFormatCorrectState();
+    }
+    return registerState;
+  }
+
+  RegisterState validateEmail(String email) {
+    RegisterState registerState;
+    final ValidationState validateState = Validator.validateEmail(email);
+
+    if (validateState == ValidationState.Empty) {
+      registerState = RegisterEmailEmptyFormatState();
+    } else if (validateState == ValidationState.Formatting) {
+      registerState = RegisterEmailInvalidFormatState();
+    } else {
+      registerState = RegisterEmailFormatCorrectState();
+    }
+    return registerState;
+  }
+
+  RegisterState validatePassword(String password) {
+    RegisterState registerState;
+    final ValidationState validateState = Validator.validatePassword(password);
+    if (validateState == ValidationState.Empty) {
+      registerState = RegisterPasswordEmptyFormatState();
+    } else if (validateState == ValidationState.Formatting) {
+      registerState = RegisterPasswordInvalidFormatState();
+    } else {
+      registerState = RegisterPasswordFormatCorrectState();
+    }
+    return registerState;
+  }
+
+  RegisterState validatePhone(String phone) {
+    RegisterState registerState;
+    final ValidationState validateState = Validator.validatePhone(phone);
+    if (validateState == ValidationState.Empty) {
+      registerState = RegisterPasswordEmptyFormatState();
+    } else if (validateState == ValidationState.Formatting) {
+      registerState = RegisterPasswordInvalidFormatState();
+    } else {
+      registerState = RegisterPasswordFormatCorrectState();
+    }
+    return registerState;
   }
 
   RegisterChangePasswordVisibilityState changePasswordVisibility() {
