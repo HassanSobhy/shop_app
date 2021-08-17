0b71bc658f90c041618f33ee5dbdd52b230035ee
HassanSobhy <hassansobhy60@gmail.com>
2021-08-17 18:42:55 +0200

    finish products bloc

---
+++ lib/modules/products/bloc/products_bloc.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:shop_app/models/categories/categories.dart';
+import 'package:shop_app/models/products/products.dart';
+import 'package:shop_app/modules/products/bloc/products_repository.dart';
+
+part 'products_event.dart';
+
+part 'products_state.dart';
+
+class ProductsBloc extends Bloc<ProductsEvent, ProductsState> {
+  final BaseProductsRepository productsRepository;
+
+  ProductsBloc(this.productsRepository) : super(const ProductsInitialState());
+
+  @override
+  Stream<ProductsState> mapEventToState(
+    ProductsEvent event,
+  ) async* {
+    if (event is GetProductDataEvent) {
+      yield const ProductsLoadingState();
+      yield await productsRepository.getProductsData(event.language);
+    } else if (event is GetCategoriesDataEvent) {
+      yield const CategoriesLoadingState();
+      yield await productsRepository.getCategoryData(event.language);
+    } else if (event is ChangeFavoriteProductEvent) {
+      yield await productsRepository.setProductFavorite(
+          event.favoriteModel, event.language);
+    }
+  }
+}
