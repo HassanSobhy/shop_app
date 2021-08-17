4a0e14d22b46b3e40dd21d063f0af06bcd309943
Amr <amrmahmoud710gmail.com>
2021-08-17 18:06:09 +0200

    add localization key

--- lib/utils/lang/app_localization_keys.dart
+++ lib/utils/lang/app_localization_keys.dart
@@ -1,4 +1,9 @@
 class LangKeys {
-  // ignore: constant_identifier_names
   static const APP_NAME = "app_name";
+  static const LOGIN = 'login';
+  static const LOGIN_TO_SEE_OFFERS = "login_to_see_offers";
+  static const EMAIL = "email";
+  static const PASSWORD = "password";
+  static const DONT_HAVE_ACCOUNT = "dont_have_account";
+  static const REGISTER = "register";
 }
