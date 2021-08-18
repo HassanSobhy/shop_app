74501da975440f83b2518e21279eb483ab68f488
HassanSobhy <hassansobhy60@gmail.com>
2021-08-18 19:54:41 +0200

    edit categories bloc

--- lib/modules/categories/bloc/categories_bloc.dart
+++ lib/modules/categories/bloc/categories_bloc.dart
@@ -1,11 +1,14 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:shop_app/models/categories/categories.dart';
 import 'package:shop_app/modules/categories/bloc/categories_repository.dart';
 
 part 'categories_event.dart';
+
 part 'categories_state.dart';
 
 class CategoriesBloc extends Bloc<CategoriesEvent, CategoriesState> {
@@ -13,6 +16,9 @@ class CategoriesBloc extends Bloc<CategoriesEvent, CategoriesState> {
 
   CategoriesBloc(this.categoriesRepository)
       : super(const CategoriesInitialState());
+
+  static CategoriesBloc get(BuildContext context) =>
+      BlocProvider.of<CategoriesBloc>(context);
 
   @override
   Stream<CategoriesState> mapEventToState(
